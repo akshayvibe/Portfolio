519d05c846d3447544e90c30362a15cf6cf85d78
akshay <akshayjha2111@gmail.com>
2026-01-12 14:12:06 +0530

    Fixed issue

--- porthat/src/router.tsx
+++ porthat/src/router.tsx
@@ -1,4 +1,4 @@
-import { createRouter } from '@tanstack/react-router'
+import { createRouter, Link } from '@tanstack/react-router' // Added Link import
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
@@ -8,9 +8,18 @@ export const getRouter = () => {
   const router = createRouter({
     routeTree,
     context: {},
-
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
+    // Add this section below
+    defaultNotFoundComponent: () => {
+      return (
+        <div style={{ padding: '2rem', textAlign: 'center' }}>
+          <h2>404 - Page Not Found</h2>
+          <p>The page you are looking for doesn't exist.</p>
+          <Link to="/">Go back to Home</Link>
+        </div>
+      )
+    },
   })
 
   return router
